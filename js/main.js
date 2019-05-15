69e3109831ea1b637f088caf658fe50233215bd3
SOyeniyi801 <47873502+SOyeniyi801@users.noreply.github.com>
2019-05-15 17:36:51 -0400

    Reset/Win logic
    added reset button
    Shmoney prints in console log when you win

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-  $("img").click(getValue);
+  $(".money-container img").click(getValue);
   let sum = 0
   function getValue(){
     let userChoice = $(this).attr('id');
@@ -8,7 +8,27 @@ $(function(){
     sum = sum + value
     console.log(sum.toFixed(2));
     $("#sum").text(sum.toFixed(2));
+
+    let questions = Number($("span").text())
+    console.log("compare bank", questions);
+
+    if(questions === sum){
+    console.log("Shmoney!");
+    } else{
+
+    }
+
+
+
+
+
   }
 
+  $(".reset").click(reset);
+  function reset(){
+    sum=0;
+    console.log("reset game");
+    $("#sum").text(sum);
+  }
 
 });
